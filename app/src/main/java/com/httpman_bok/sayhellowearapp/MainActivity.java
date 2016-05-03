dfb7fe9eb44ac2a32509de10afcf9b3d850d5f2a
jinmanbok <jinmanbok518@hotmail.com>
2016-05-02 23:55:39 -0700

    Initial commit

---
+++ app/src/main/java/com/httpman_bok/sayhellowearapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.httpman_bok.sayhellowearapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    public void sayHello (View view) {
+        Toast.makeText(getApplicationContext(), "Hi!", Toast.LENGTH_LONG).show();
+
+
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+
+
+
+
+
+            }
+        });
+    }
+}
